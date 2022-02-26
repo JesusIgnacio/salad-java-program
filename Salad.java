ae03cd84f8d5f0ba176ff9218de702bbff867d81
jesus.castillo <jesus.castillo@bnamericas.com>
2022-02-26 11:45:29 -0300

    ad sources example

---
+++ Salad.java
@@ -0,0 +1,9 @@
+public class Salad {
+	
+	public static Watermelon watermelon = new Watermelon();
+
+	public static void main(String[] args) {	
+		watermelon.talk();
+		System.out.println("I'm the best salad in this neighberhood!");
+	}
+}
---
+++ Watermelon.java
@@ -0,0 +1,6 @@
+public class Watermelon {
+	
+	public void talk() {
+		System.out.println("I'm the best fruit!");
+	}
+}
